d9ad251dae6813fc5909761da69dbae91a9164fe
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2015-12-09 09:49:18 +0100

    Changed user in gap_options script

--- install_scripts/install_gap_options.sh
+++ install_scripts/install_gap_options.sh
@@ -7,7 +7,7 @@ number_cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 mkdir -p /opt2/gap/local/pkg
 
 sudo touch /usr/bin/gap
-sudo chown spp:spp /usr/bin/gap
+sudo chown jenkins:jenkins /usr/bin/gap
 sudo chmod 0755 /usr/bin/gap
 cat > /usr/bin/gap <<EOF
 #!/bin/bash
@@ -15,15 +15,15 @@ cat > /usr/bin/gap <<EOF
 EOF
 
 sudo touch /usr/bin/gapL
-sudo chown spp:spp /usr/bin/gapL
+sudo chown jenkins:jenkins /usr/bin/gapL
 sudo chmod 0755 /usr/bin/gapL
 cat > /usr/bin/gapL <<EOF
 #!/bin/bash
 /opt2/gap/bin/gap.sh -l "/opt2/gap/local;/opt2/gap" -L /opt2/gap/bin/wsgap4 "\$@"
 EOF
 
-mkdir /home/spp/.gap
-cat > /home/spp/.gap/gap.ini <<EOF
+mkdir /home/jenkins/.gap
+cat > /home/jenkins/.gap/gap.ini <<EOF
 SetUserPreference( "UseColorPrompt", true );
 SetUserPreference( "UseColorsInTerminal", true );
 SetUserPreference( "HistoryMaxLines", 10000 );
